a303734afb0fe37f6ba7fa6e6f3d69ff1f9345ae
Bhaskara Gowda <mohan.permude@gmail.com>
2024-10-31 17:12:50 +0530

    read me file amd authenticate and swaager changes

--- app/Models/UserPreference.php
+++ app/Models/UserPreference.php
@@ -5,6 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @OA\Schema(
+ *     schema="UserPreference",
+ *     type="object",
+ *     @OA\Property(property="user_id", type="integer", description="User ID"),
+ *     @OA\Property(property="preferred_sources", type="array", @OA\Items(type="string"), description="Preferred news sources"),
+ *     @OA\Property(property="preferred_categories", type="array", @OA\Items(type="string"), description="Preferred news categories"),
+ *     @OA\Property(property="preferred_authors", type="array", @OA\Items(type="string"), description="Preferred authors")
+ * )
+ */
 class UserPreference extends Model
 {
     use HasFactory;
